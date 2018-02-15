0fdf10dec89ca5984d2dc86fa4e0ff29fee272e6
rafael.menendez <rafael.menendez@meteologica.com>
2018-02-15 10:55:59 +0000

    Primeras mejoras

--- scrapping_pdfs.R
+++ scrapping_pdfs.R
@@ -4,7 +4,7 @@ library(raster)
 library(ggplot2)
 library(ggthemes)
 library(tidyr)
-# library(verification)
+library(verification)
 library(lubridate)
 library(meteologica)
 library(pdftools)
@@ -16,8 +16,9 @@ readPDFobservaciones <- function(tipo="CUPR",fecha){
   pdf <- readPDF(control=list(text="-table"))
   pdftxt <- pdf(elem = list(uri=sprintf("attach/%s/%s.pdf",fecha,tipo)),language = "Spanish")
   pdftxt <- content(pdftxt)
-  puntos <- c("Alcalá","Aranjuez","Collado","Coslada","Getafe","Las Rozas","Madrid Barrio","Madrid-Ciudad","Media")
-  cabecera <- pdftxt[23]
+  pdftxt <- strsplit(pdftxt,"\n")[[1]]
+  puntos <- c("Alcalá","Aranjuez","Collado","Coslada","Getafe","Las Rozas","Salamanca","Universitaria","Media")
+  cabecera <- pdftxt[15]
   cut <- sapply(puntos, FUN=function(x) {gregexpr(x,cabecera)[[1]][1]})
   #quienes tienen y quienes no
   puntos.no <- names(which(cut==-1))
@@ -25,7 +26,8 @@ readPDFobservaciones <- function(tipo="CUPR",fecha){
   cut <- c(98,cut[cut!=-1])-1
   posiciones <- diff(cut)
   
-  ln <- c(27,29,31,33,35,37,39)
+  #ln <- c(27,29,31,33,35,37,39)
+  ln <- c(16,17,18,19,20,21,22)
   datos <- pdftxt[ln]
   datos <- gsub("^\\s","",datos)
   datos.for <- list()
@@ -43,12 +45,55 @@ readPDFobservaciones <- function(tipo="CUPR",fecha){
       colnames(datos) <- c(colnames(datos)[-ncol(datos)],p)
     }
   }
+  datos <- datos[!is.na(datos$fechas),]
   datos$fechas <- as.Date(datos$fechas,format="%d/%m/%y")
+  
   return(datos)
   } else {
     print("Ese archivo no existe")
   }
 }
+
+readPDFobservaciones_tabulizer <- function(tipo="CUPR",fecha){ 
+  if(file.exists(sprintf("attach/%s/%s.pdf",fecha,tipo))){
+    pdffile <- sprintf("attach/%s/%s.pdf",fecha,tipo)
+    table <- extract_tables(file = pdffile)[[1]]
+    coltables <- table[1,]
+    #si hay columna media la quito porque da problemas y la puedo calcular a posterior
+    if ("media" %in% tolower(coltables)){
+      table <- table[,-which(tolower(coltables)=="media")]
+    } else {
+      #si no hay es la ultima columna
+      table <- table[,-ncol(table)]
+    }
+    coltables <- c("fechas",table[1,])
+    #si hay alguna columna con todo vacio la quitamos
+    columnas <- which(apply(table,2,FUN=function(x) sum(x!=""))==0)
+    if (length(columnas)!=0){
+      table <- table[,-columnas]
+    }
+    filas <- which(apply(table,1,FUN=function(x) sum(x!=""))==0)
+    if (length(filas)!=0){
+      table <- table[-filas,]
+    }
+    coltables <- coltables[coltables!=""]
+    table <- as.data.frame(table[-1,])
+    colnames(table) <- coltables
+    
+    #un poco de tuneo de los nombre de las columnas
+    cols <- colnames(table)
+    cols <- gsub(pattern = "Media",replacement = "",cols)
+    cols <- gsub(pattern = 'Madrid-.* ',"",cols)
+    cols[cols=="Madrid-Arganzuela"] <- "Arganzuela"
+    cols[cols=="Barrio"] <- "Salamanca"
+    cols[cols=="Ciudad"] <- "Universitaria"
+    colnames(table) <- cols
+    return(table)
+  } else {
+    print("Ese archivo no existe")
+  }
+}
+
 
 SerieObservaciones <- function(tipo="CUPR"){
   lf <- list.dirs("attach/",full.names = FALSE)
@@ -56,7 +101,8 @@ SerieObservaciones <- function(tipo="CUPR"){
   n <- 1
   for (l in lf){
     print(l)
-    o <- readPDFobservaciones(fecha=l)
+    #o <- readPDFobservaciones(fecha=l)
+    o <- readPDFobservaciones_tabulizer(fecha=l)
     if (is.data.frame(o)){
       obs[[n]] <- o
       n <- n+1
@@ -64,8 +110,9 @@ SerieObservaciones <- function(tipo="CUPR"){
   }
   fechas <- unique(bind_rows(obs)$fechas)
   fechas <- fechas[!is.na(fechas)]
-  obs.def <- as.data.frame(matrix(NA,nrow=length(fechas),ncol=ncol(obs[[1]])))
-  colnames(obs.def) <- colnames(obs[[1]])
+  cols <- colnames(obs[[which(sapply(obs, ncol)==max(sapply(obs, ncol)))[1]]])
+  obs.def <- as.data.frame(matrix(NA,nrow=length(fechas),ncol=length(cols)))
+  colnames(obs.def) <- cols
   obs.def$fechas <- fechas
   #relleno el pavo
   for (i in 1:length(obs)){
@@ -73,11 +120,14 @@ SerieObservaciones <- function(tipo="CUPR"){
     for (r in 1:nrow(obs[[i]])){
       o <- obs[[i]][r,]
       if(!is.na(o$fecha)){
-      print(o$fecha)
-      obs.def[obs.def$fechas==o$fecha,-1] <- as.integer(o[,-1])
+      #print(o$fecha)
+      columnas.quetengo <- colnames(o)[colnames(o) %in% cols]
+      columnas.quetengo <- columnas.quetengo[columnas.quetengo!="fechas"]
+      obs.def[obs.def$fechas==o$fecha,columnas.quetengo] <- as.integer(o[,columnas.quetengo])
       }
     }
   }
+  obs.def$media <- rowMeans(obs.def[,-1],na.rm = TRUE)
   return(obs.def)
 }
 
@@ -99,16 +149,23 @@ readPrediccionesPDF <- function(tipo="CUPR",fecha){
   valores <- character()
   lf.jpg <- list.files(path=".",pattern = ".jpg")
   lf.jpg <- lf.jpg[!grepl("0000.jpg",lf.jpg)]
+  lf.jpg <- lf.jpg[!grepl("000.jpg",lf.jpg)]
   for (n in 1:4){
     colores[[n]] <- raster::extract(stack(lf.jpg[n]),cbind(0,0))
     if (sum(colores[[n]]==c(153,1,52))==3) {valores[n] <- "MA"}
     if (sum(colores[[n]]==c(254,153,0))==3) {valores[n] <- "A"}
     if (sum(colores[[n]]==c(0,129,2))==3) {valores[n] <- "M"}
+    if (sum(colores[[n]]==c(0,128,1))==3) {valores[n] <- "M"}
     if (sum(colores[[n]]==c(48,0,100))==3) {valores[n] <- "B"}
     if (sum(colores[[n]]==c(255,193,214))==3) {valores[n] <- "MA"}
     if (sum(colores[[n]]==c(255,197,11))==3) {valores[n] <- "A"}
     if (sum(colores[[n]]==c(178,215,110))==3) {valores[n] <- "M"}
+    if (sum(colores[[n]]==c(50,2,102))==3) {valores[n] <- "B"}
+    if (sum(colores[[n]]==c(51,0,101))==3) {valores[n] <- "B"}
     if (sum(colores[[n]]==c(236,217,245))==3) {valores[n] <- "B"}
+    if (sum(colores[[n]]==c(237,217,245))==3) {valores[n] <- "B"}
+    if (sum(colores[[n]]==c(62,79,105))==3) {valores[n] <- "B"}
+    if (sum(colores[[n]]==c(61,164,75))==3) {valores[n] <- "M"}
   }
   unlink(lf.jpg)
   return(valores)
@@ -138,8 +195,9 @@ SeriePredicciones <- function(tipo="CUPR"){
 
 CreaSerieMedia <- function(tipo="CUPR"){
   obs <- SerieObservaciones()
-  obs <- obs[,c("fechas","Media")]
-  obs <- obs %>% dplyr::group_by(dia=yday(fechas)) %>% dplyr::summarise(media=mean(Media,na.rm=TRUE))
+  obs <- obs[,c("fechas","media")]
+  obs$fechas <- as.Date(obs$fechas,forma="%d/%M/%y")
+  obs <- obs %>% dplyr::group_by(dia=yday(fechas)) %>% dplyr::summarise(media=mean(media,na.rm=TRUE))
   obs$cat <- cut(obs$media,breaks=c(0,150,300,600,5000),labels=c("B","M","A","MA"))
   obs$cat <- as.character(obs$cat)
   return(obs)
@@ -154,6 +212,7 @@ AnalizaPredicciones <- function(preds=preds,obs=obs){
   #rellenamos con un bucle poco elegante
   preds.comp$media <- NA
   for (i in 1:nrow(preds.comp)){
+    print(i)
     if(length(obs[obs$dia==yday(preds.comp[i,]$fecha),]$cat)!=0){
     preds.comp[i,]$media <- obs[obs$dia==yday(preds.comp[i,]$fecha),]$cat
     }
@@ -208,3 +267,8 @@ AnalizaPredicciones <- function(preds=preds,obs=obs){
   print("GS para media")
   print(multi.cont(media)$gs)
 }
+
+obs <- SerieObservaciones()
+preds <- SeriePredicciones()
+obs2 <- CreaSerieMedia()
+AnalizaPredicciones(obs = obs2,preds = preds)
